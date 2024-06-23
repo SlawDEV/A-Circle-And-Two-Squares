913516cf529cd74b2020aa8e5c3a8e4ff16f3cc4
SlawDEV <148549968+SlawDEV@users.noreply.github.com>
2024-06-23 20:01:42 +0200

    Add files via upload

---
+++ ACircleAndTwoSquares/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Function, that takes an integer (radius of the circle) and returns the difference of the areas of the two squares.
+ For the smaller one, the circle is a circumcircle and for the bigger one, an incircle.
+ */
+
+using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(square_areas_difference(5));
+            Console.WriteLine(square_areas_difference(6));
+            Console.WriteLine(square_areas_difference(7));
+
+            Console.ReadKey();
+        }
+
+        static int square_areas_difference(int radius)
+        {
+            return ((2 * radius) *(2 * radius)) - (2*(radius*radius));
+        }
+    }
+}
